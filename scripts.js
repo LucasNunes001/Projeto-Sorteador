3969e432c88b41497949e7a1b5b67fe2c9035042
LucasNunes001 <lucasalmeida_nunes@hotmail.com>
2026-03-25 01:27:48 -0300

    condição criada no js e pequena alteração no html

--- scripts.js
+++ scripts.js
@@ -1,18 +1,23 @@
-const buttonGerenate = document.querySelector('.button-generate') // variavel do botão 'sortear' 
-function generateNumber() { // função para gerar o numero aleatorio
+const buttonGerenate = document.querySelector('.button-generate')  // variavel do botão 'sortear' 
+function generateNumber() {  // função para gerar o numero aleatorio
 
-    const min = Math.ceil(document.querySelector('.input-min').value) // variavel do input minimo, para pegar o valor digitado e arredondar para cima, caso o numero seja decimal
-    const max = Math.floor(document.querySelector('.input-max').value) // variavel do input maximo, para pegar o valor digitado e arredondar para baixo, caso o numero seja decimal
+    const min = Math.ceil(document.querySelector('.input-min').value)  // variavel do input minimo, para pegar o valor digitado e arredondar para cima, caso o numero seja decimal
+    const max = Math.floor(document.querySelector('.input-max').value)  // variavel do input maximo, para pegar o valor digitado e arredondar para baixo, caso o numero seja decimal
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + Number(min) // variavel com a formula para gerar o numero aleatorio, arredondando para baixo, multiplicando pelo intervalo (max - min + 1) e somando o valor minimo para garantir que o numero gerado esteja dentro do intervalo definido
+     if (max <= min) {  // se o numero maximo for menor ou igual ao numero minimo, entra aqui para exibir um alerta e definir o resultado como '...' para indicar q nao foi gerado um numero, e exibe erro no console
+        alert('O número máximo deve ser maior que o número mínimo.')
+        document.getElementById('result').innerText = '...' 
+        
+        console.log('Erro: o número máximo deve ser maior que o número mínimo.')
+    } else {  // se nao for, ele gera o numero aleatorio normalmente
+        const result = Math.floor(Math.random() * (max - min + 1)) + Number(min)   // variavel com a formula para gerar o numero aleatorio, arredondando para baixo, multiplicando pelo intervalo dos inputs (max - min + 1) e somando o valor minimo para garantir que o numero gerado esteja dentro do intervalo definido
+        document.getElementById('result').innerText = result   // seleciona o elemento com id 'result' e define seu texto para o numero gerado, para exibir o resultado na tela
+        
+        console.log('sorteando...')
+        console.log('Número sorteado:' + result)
+    }
 
-
-    document.getElementById('result').innerText = result // seleciona o elemento com id 'result' e define seu texto como o numero gerado, para exibir o resultado na tela
-
-
-
-    console.log('sorteando...')
-    console.log(result)
+    
 }
 
 
